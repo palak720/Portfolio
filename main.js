35ec157f2f18ced982ce00fbeb3bfe33e29a2265
Palakkasaudhan <Palakkasaudhan123@gamil.com>
2025-02-13 11:58:15 +0530

    Portfolio

--- main.js
+++ main.js
@@ -5,26 +5,13 @@
     backDelay:1000,
     loop:true,
 })*/
-// Initialize EmailJS with your Public Key
-emailjs.init('YOUR_PUBLIC_KEY');
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault();
+  
+/* js for navbar toggle*/
+const menubtn = document.querySelector('.menu-btn');
+    const navbar = document.querySelector('.navbar');
 
-  const form = this;
-
-  // Send the form data using EmailJS
-  emailjs.sendForm('service_5v63rlv', 'template_cuj094c', form)
-    .then(() => {
-      // Show success message
-      document.getElementById('success-message').textContent = "Message sent successfully!";
-      document.getElementById('success-message').style.display = "block";
-
-      // Clear the form
-      form.reset();
-    }, (error) => {
-      console.error('Failed to send the message:', error);
-      document.getElementById('success-message').textContent = "Failed to send the message. Please try again.";
-      document.getElementById('success-message').style.display = "block";
+    document.querySelector('.menu-btn').addEventListener('click', () => {
+        document.querySelector('.navbar').classList.toggle('active');
     });
-});
+    
